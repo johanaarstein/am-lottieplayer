93d8602af76d1e9435be3bd8b99a8223ee577a2b
johanaarstein <johan.aarstein@sempro.no>
2022-11-04 22:19:39 +0100

    ..

--- build/index.asset.php
+++ build/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => 'c336991cf16b06081111');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-element', 'wp-i18n'), 'version' => '6ff23a66e8dae606ea23');
