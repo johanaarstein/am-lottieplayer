6fdffd4d9ad4ea2f354d8d15c222b6bf38408673
Johan Martin Aarstein <johan.aarstein@gmail.com>
2022-11-16 20:30:26 +0100

    ..

--- src/components/Placeholder.js
+++ src/components/Placeholder.js
@@ -64,35 +64,6 @@ const UploadComponent = ( {
 	);
 };
 
-// const UploadComponent = ( { setAttributes = () => {} } ) => {
-// 	return (
-// 		<div className={ `components-placeholder` }>
-// 			<div className="components-placeholder__label">
-// 				<Lottie /> { __( 'Lottie Animation' ) }
-// 			</div>
-// 			<div className="components-placeholder__instructions">
-// 				{ __(
-// 					'Upload Lottie animations to WordPress and add them in Gutenberg.'
-// 				) }
-// 			</div>
-// 			<div className="components-placeholder__fieldset">
-// 				<form>
-// 					<Upload
-// 						onSelect={ ( newSrc ) =>
-// 							setAttributes( { src: newSrc } )
-// 						}
-// 						render={ ( { open } ) => (
-// 							<Button variant="primary" onClick={ open }>
-// 								{ __( 'Media Library' ) }
-// 							</Button>
-// 						) }
-// 					/>
-// 				</form>
-// 			</div>
-// 		</div>
-// 	);
-// };
-
 const PlayerComponent = ( { attributes = {} } = {} ) => {
 	const {
 			align,
