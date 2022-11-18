753c928003a188405460390736e86317abccad45
johanaarstein <johan.aarstein@sempro.no>
2022-11-18 18:03:52 +0100

    ..

--- includes/modules/LottiePlayer/index.php
+++ includes/modules/LottiePlayer/index.php
@@ -89,34 +89,34 @@ if (!class_exists('AM_ET_Builder_Module_LottiePlayer')) {
 		function get_fields() {
 			$fields = [
 				'src' => [
-					'label' => esc_html__('Lottie', 'lottieplayer'),
+					'label' => esc_html__('Lottie', 'am_lottieplayer'),
 					'type' => 'upload',
 					'option_category' => 'basic_option',
 					'data_type' => ['application/zip', 'application/json', 'text/plain'],
-					'upload_button_text' => esc_attr__('Use animation', 'lottieplayer'),
-					'choose_text' => esc_attr__('Choose a Lottie JSON or a dotLottie', 'lottieplayer'),
-					'update_text'  => esc_attr__('Set As Lottie', 'lottieplayer'),
+					'upload_button_text' => esc_attr__('Use animation', 'am_lottieplayer'),
+					'choose_text' => esc_attr__('Choose a Lottie JSON or a dotLottie', 'am_lottieplayer'),
+					'update_text'  => esc_attr__('Set As Lottie', 'am_lottieplayer'),
 					'hide_metadata' => true,
 					'affects' => ['alt', 'title_text'],
-					'description' => esc_html__('Upload your desired animation in Lottie JSON format, dotLottie format, or type in the URL to the Lottie you would like to display', 'lottieplayer'),
+					'description' => esc_html__('Upload your desired animation in Lottie JSON format, dotLottie format, or type in the URL to the Lottie you would like to display', 'am_lottieplayer'),
 					'toggle_slug' => 'main_content',
 					'mobile_options' => true,
 					'hover' => 'tabs',
 					'default' => 'https://assets8.lottiefiles.com/packages/lf20_kjclizjz.json',
 				],
 				'alt' => [
-					'label' => esc_html__('Animation Alternative Text', 'lottieplayer'),
+					'label' => esc_html__('Animation Alternative Text', 'am_lottieplayer'),
 					'type' => 'text',
 					'option_category' => 'basic_option',
 					'depends_show_if' => 'on',
 					'depends_on' => ['src'],
-					'description' => esc_html__('This defines the HTML ALT text. A short description of your animation can be placed here. Helpful for screen readers.', 'lottieplayer'),
+					'description' => esc_html__('This defines the HTML ALT text. A short description of your animation can be placed here. Helpful for screen readers.', 'am_lottieplayer'),
 					'tab_slug' => 'custom_css',
 					'toggle_slug' => 'attributes',
 					'dynamic_content' => 'text',
 				],
 				'title_text' => [
-					'label' => esc_html__('Animation Title Text', 'lottieplayer'),
+					'label' => esc_html__('Animation Title Text', 'am_lottieplayer'),
 					'type'  => 'text',
 					'option_category' => 'basic_option',
 					'depends_show_if' => 'on',
@@ -140,19 +140,19 @@ if (!class_exists('AM_ET_Builder_Module_LottiePlayer')) {
 				// 		'url_new_window',
 				// 	],
 				// 	'toggle_slug'   => 'link',
-				// 	'description'   => esc_html__('Here you can choose whether or not the Lottie should open in Lightbox. Note: if you select to open the Lottie in Lightbox, url options below will be ignored.', 'lottieplayer'),
+				// 	'description'   => esc_html__('Here you can choose whether or not the Lottie should open in Lightbox. Note: if you select to open the Lottie in Lightbox, url options below will be ignored.', 'am_lottieplayer'),
 				// ],
 				'url'      => [
-					'label'        => esc_html__('Animation Link URL', 'lottieplayer'),
+					'label'        => esc_html__('Animation Link URL', 'am_lottieplayer'),
 					'type' => 'text',
 					'option_category' => 'basic_option',
 					'depends_show_if' => 'off',
-					'description'  => esc_html__('If you would like your Lottie to be a link, input your destination URL here. No link will be created if this field is left blank.', 'lottieplayer'),
+					'description'  => esc_html__('If you would like your Lottie to be a link, input your destination URL here. No link will be created if this field is left blank.', 'am_lottieplayer'),
 					'toggle_slug'  => 'link',
 					'dynamic_content' => 'url',
 				],
 				'url_new_window'   => [
-					'label' => esc_html__('Animation Link Target', 'lottieplayer'),
+					'label' => esc_html__('Animation Link Target', 'am_lottieplayer'),
 					'type'  => 'select',
 					'option_category'  => 'configuration',
 					'options'       => [
@@ -165,7 +165,7 @@ if (!class_exists('AM_ET_Builder_Module_LottiePlayer')) {
 					'description'   => esc_html__('Here you can choose whether or not your link opens in a new window', 'et_builder'),
 				],
 				// 'use_overlay'      => [
-				// 	'label' => esc_html__('Animation Overlay', 'lottieplayer'),
+				// 	'label' => esc_html__('Animation Overlay', 'am_lottieplayer'),
 				// 	'type'  => 'yes_no_button',
 				// 	'option_category'  => 'layout',
 				// 	'options'       => [
@@ -183,7 +183,7 @@ if (!class_exists('AM_ET_Builder_Module_LottiePlayer')) {
 				// 	],
 				// 	'tab_slug'      => 'advanced',
 				// 	'toggle_slug'   => 'overlay',
-				// 	'description'   => esc_html__('If enabled, an overlay color and icon will be displayed when a visitors hovers over the animation', 'lottieplayer'),
+				// 	'description'   => esc_html__('If enabled, an overlay color and icon will be displayed when a visitors hovers over the animation', 'am_lottieplayer'),
 				// ],
 				// 'overlay_icon_color'  => [
 				// 	'label'        => esc_html__('Overlay Icon Color', 'et_builder'),
@@ -220,7 +220,7 @@ if (!class_exists('AM_ET_Builder_Module_LottiePlayer')) {
 				// 	'sticky' => true,
 				// ],
 				'show_bottom_space'   => [
-					'label' => esc_html__('Show Space Below The Animation', 'lottieplayer'),
+					'label' => esc_html__('Show Space Below The Animation', 'am_lottieplayer'),
 					'type'  => 'yes_no_button',
 					'option_category'  => 'layout',
 					'options'       => [
@@ -230,24 +230,24 @@ if (!class_exists('AM_ET_Builder_Module_LottiePlayer')) {
 					'default_on_front' => 'on',
 					'tab_slug'      => 'advanced',
 					'toggle_slug'   => 'margin_padding',
-					'description'   => esc_html__('Here you can choose whether or not the animation should have a space below it.', 'lottieplayer'),
+					'description'   => esc_html__('Here you can choose whether or not the animation should have a space below it.', 'am_lottieplayer'),
 					'mobile_options'   => true,
 				],
 				// 'align'    => [
-				// 	'label' => esc_html__('Lottie Alignment', 'lottieplayer'),
+				// 	'label' => esc_html__('Lottie Alignment', 'am_lottieplayer'),
 				// 	'type'  => 'text_align',
 				// 	'option_category'  => 'layout',
 				// 	'options'       => et_builder_get_text_orientation_options(['justified']),
 				// 	'default_on_front' => 'left',
 				// 	'tab_slug'      => 'advanced',
 				// 	'toggle_slug'   => 'alignment',
-				// 	'description'   => esc_html__('Here you can choose the Lottie alignment.', 'lottieplayer'),
+				// 	'description'   => esc_html__('Here you can choose the Lottie alignment.', 'am_lottieplayer'),
 				// 	'options_icon'  => 'module_align',
 				// 	'mobile_options'   => true,
 				// ],
 				'force_fullwidth'  => [
 					'label' => esc_html__('Force Fullwidth', 'et_builder'),
-					'description'   => esc_html__("When enabled, this will force your animation to extend 100% of the width of the column it's in.", 'lottieplayer'),
+					'description'   => esc_html__("When enabled, this will force your animation to extend 100% of the width of the column it's in.", 'am_lottieplayer'),
 					'type'  => 'yes_no_button',
 					'option_category'  => 'layout',
 					'options'       => [
@@ -263,8 +263,8 @@ if (!class_exists('AM_ET_Builder_Module_LottiePlayer')) {
 					],
 				],
 				'autoplay' => [
-					'label' => esc_html__('Autoplay', 'lottieplayer'),
-					'description' => esc_html__('Control whether the animation plays on page load or not.', 'lottieplayer'),
+					'label' => esc_html__('Autoplay', 'am_lottieplayer'),
+					'description' => esc_html__('Control whether the animation plays on page load or not.', 'am_lottieplayer'),
 					'type' => 'yes_no_button',
 					'option_category' => 'basic_option',
 					'options'  => [
@@ -275,8 +275,8 @@ if (!class_exists('AM_ET_Builder_Module_LottiePlayer')) {
 					'toggle_slug' => 'main_content',
 				],
 				'controls' => [
-					'label' => esc_html__('Controls', 'lottieplayer'),
-					'description' => esc_html__('Show or hide controls.', 'lottieplayer'),
+					'label' => esc_html__('Controls', 'am_lottieplayer'),
+					'description' => esc_html__('Show or hide controls.', 'am_lottieplayer'),
 					'type' => 'yes_no_button',
 					'option_category' => 'basic_option',
 					'options'  => [
@@ -287,8 +287,8 @@ if (!class_exists('AM_ET_Builder_Module_LottiePlayer')) {
 					'toggle_slug' => 'main_content',
 				],
 				'loop' => [
-					'label' => esc_html__('Loop', 'lottieplayer'),
-					'description' => esc_html__('Loop the animation.', 'lottieplayer'),
+					'label' => esc_html__('Loop', 'am_lottieplayer'),
+					'description' => esc_html__('Loop the animation.', 'am_lottieplayer'),
 					'type' => 'yes_no_button',
 					'option_category' => 'basic_option',
 					'options'  => [
@@ -299,27 +299,27 @@ if (!class_exists('AM_ET_Builder_Module_LottiePlayer')) {
 					'toggle_slug' => 'main_content',
 				],
 				'object_fit' => [
-					'label' => esc_html__('Object Fit', 'lottieplayer'),
-					'description' => esc_html__('Choose how to scale the animation', 'lottieplayer'),
+					'label' => esc_html__('Object Fit', 'am_lottieplayer'),
+					'description' => esc_html__('Choose how to scale the animation', 'am_lottieplayer'),
 					'type' => 'select',
 					'option_category' => 'basic_option',
 					'options'  => [
-						'contain' => esc_html__('Contain', 'lottieplayer'),
-						'cover' => esc_html__('Cover', 'lottieplayer'),
-						'fill' => esc_html__('Fill', 'lottieplayer'),
-						'none' => esc_html__('None', 'lottieplayer'),
+						'contain' => esc_html__('Contain', 'am_lottieplayer'),
+						'cover' => esc_html__('Cover', 'am_lottieplayer'),
+						'fill' => esc_html__('Fill', 'am_lottieplayer'),
+						'none' => esc_html__('None', 'am_lottieplayer'),
 					],
 					'default' => 'contain',
 					'toggle_slug' => 'main_content',
 				],
 				'renderer' => [
-					'label' => esc_html__('Renderer', 'lottieplayer'),
-					'description' => esc_html__('Choose renderer', 'lottieplayer'),
+					'label' => esc_html__('Renderer', 'am_lottieplayer'),
+					'description' => esc_html__('Choose renderer', 'am_lottieplayer'),
 					'type' => 'select',
 					'option_category' => 'configuration',
 					'options'  => [
-						'svg' => esc_html__('SVG', 'lottieplayer'),
-						'canvas' => esc_html__('Canvas', 'lottieplayer'),
+						'svg' => esc_html__('SVG', 'am_lottieplayer'),
+						'canvas' => esc_html__('Canvas', 'am_lottieplayer'),
 					],
 					'default' => 'svg',
 					// 'tab_slug' => 'advanced',
@@ -380,4 +380,3 @@ if (!class_exists('AM_ET_Builder_Module_LottiePlayer')) {
 
 	new AM_ET_Builder_Module_LottiePlayer;	
 }
-
