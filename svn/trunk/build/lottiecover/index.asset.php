da6ab0a916176ed7091ce2f0ed1211508b2c9eeb
johanaarstein <johan.aarstein@sempro.no>
2023-03-03 09:52:10 +0100

    updated scripts

--- svn/trunk/build/lottiecover/index.asset.php
+++ svn/trunk/build/lottiecover/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2efe6ed22aeea960189d');
+<?php return array('dependencies' => array('wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '6603df58c035f294e6bb');
