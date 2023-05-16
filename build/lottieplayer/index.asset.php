dd5a51323ce4409b2f3678c8249b49ed983cc835
johanaarstein <johan.aarstein@sempro.no>
2023-05-16 22:47:11 +0200

    pkg

--- build/lottieplayer/index.asset.php
+++ build/lottieplayer/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5829c8191e53e87f41d2');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '0c2eb12164c71b2574f0');
