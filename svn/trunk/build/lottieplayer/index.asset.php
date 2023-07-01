97fe6e9a86039b2a2a9435bae647696a01e0b3f7
Johan Martin Aarstein <johan.aarstein@gmail.com>
2023-07-01 21:17:41 +0200

    pkg

--- svn/trunk/build/lottieplayer/index.asset.php
+++ svn/trunk/build/lottieplayer/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e5cc1953b7408ef022a8');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '393753058a86d8ec427b');
