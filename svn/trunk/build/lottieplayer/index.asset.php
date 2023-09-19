2df192b90333c5912a785eaed1a47112fd309016
johanaarstein <johan@aarstein.media>
2023-09-19 13:16:23 +0200

    update

--- svn/trunk/build/lottieplayer/index.asset.php
+++ svn/trunk/build/lottieplayer/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '3692e2d66368e51060fd');
+<?php return array('dependencies' => array('wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '5d093439528bb1ec5aec');
