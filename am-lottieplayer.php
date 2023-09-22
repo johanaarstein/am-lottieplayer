bca55200c472b62e75c8813af5b0c978f5c53d9b
Johan Martin Aarstein <johan@aarstein.media>
2023-09-22 14:02:28 +0200

    notice

--- am-lottieplayer.php
+++ am-lottieplayer.php
@@ -63,6 +63,9 @@ if (!class_exists('AM_LottiePlayer')) {
       add_action('after_setup_theme', [$this, 'init_flatsome']);
       add_action('vc_before_init', [$this, 'init_vc']);
 
+      // Check if other instances are active
+      add_action('activated_plugin', array($this, 'deactivate_other_instances'));
+      add_action('pre_current_active_plugins', array($this, 'plugin_deactivated_notice'));
     }
 
     /**
@@ -235,6 +238,68 @@ if (!class_exists('AM_LottiePlayer')) {
       am_include('includes/vc/vc-am-lottieplayer.php');
     }
 
+    /**
+     * Checks if another version of AM LottiePlayer/AM LottiePlayer PRO is active and deactivates it.
+     * Hooked on `activated_plugin` so other plugin is deactivated when current plugin is activated.
+     *
+     * @param string $plugin The plugin being activated.
+     * @return void
+     */
+    public function deactivate_other_instances($plugin)
+    {
+      if (!in_array($plugin, ['am-lottieplayer/am-lottieplayer.php', 'am-lottieplayer-pro/am-lottieplayer-pro.php'], true)) {
+        return;
+      }
+
+      $plugin_to_deactivate  = 'am-lottieplayer/am-lottieplayer.php';
+      $deactivated_notice_id = '1';
+
+      // If we just activated the free version, deactivate the pro version.
+      if ($plugin === $plugin_to_deactivate) {
+        $plugin_to_deactivate  = 'am-lottieplayer-pro/am-lottieplayer-pro.php';
+        $deactivated_notice_id = '2';
+      }
+
+      if (is_multisite() && is_network_admin()) {
+        $active_plugins = (array) get_site_option('active_sitewide_plugins', []);
+        $active_plugins = array_keys($active_plugins);
+      } else {
+        $active_plugins = (array) get_option('active_plugins', []);
+      }
+
+      foreach ($active_plugins as $plugin_basename) {
+        if ($plugin_to_deactivate === $plugin_basename) {
+          set_transient('am_deactivated_notice_id', $deactivated_notice_id, 1 * HOUR_IN_SECONDS);
+          deactivate_plugins($plugin_basename);
+          return;
+        }
+      }
+    }
+
+    /**
+     * Displays a notice when either Am LottiePlayer or Am LottiePlayer PRO is automatically deactivated.
+     * @return void
+     */
+    public function plugin_deactivated_notice()
+    {
+      $deactivated_notice_id = (int) get_transient('am_deactivated_notice_id');
+      if (!in_array($deactivated_notice_id, [1, 2], true)) {
+        return;
+      }
+
+      $message = __('AM LottiePlayer and AM LottiePlayer  PRO should not be active at the same time. We\'ve automatically deactivated AM LottiePlayer.', 'am-lottieplayer');
+      if (2 === $deactivated_notice_id) {
+        $message = __('AM LottiePlayer and AM LottiePlayer PRO should not be active at the same time. We\'ve automatically deactivated AM LottiePlayer PRO.', 'am-lottieplayer');
+      }
+
+?>
+      <div class="updated" style="border-left: 4px solid #ffba00;">
+        <p><?php echo esc_html($message); ?></p>
+      </div>
+<?php
+
+      delete_transient('am_deactivated_notice_id');
+    }
   }
 
   /**
@@ -255,4 +320,3 @@ if (!class_exists('AM_LottiePlayer')) {
 
   am_lottieplayer();
 } // class_exists check end
-
