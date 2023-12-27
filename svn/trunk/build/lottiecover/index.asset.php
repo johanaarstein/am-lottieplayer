2a7dab05d82ab3cec9cf88ca0fac9666067a907c
johanaarstein <johan@aarstein.media>
2023-12-27 16:21:16 +0100

    defer

--- svn/trunk/build/lottiecover/index.asset.php
+++ svn/trunk/build/lottiecover/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'e641ea339061d21fce2d');
+<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2de5b538c6155cf48bca');
