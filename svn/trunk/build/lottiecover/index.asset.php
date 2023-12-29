ec0b40be1199b947c491d5c46e3e2162b34523ff
johanaarstein <johan@aarstein.media>
2023-12-29 19:17:35 +0100

    stash

--- svn/trunk/build/lottiecover/index.asset.php
+++ svn/trunk/build/lottiecover/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '2de5b538c6155cf48bca');
+<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f00891ce331fd705825c');
