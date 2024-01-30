54b00cd4825e9014e4550691cf066c54ed865feb
Johan Martin Aarstein <johan@aarstein.media>
2024-01-30 20:45:48 +0100

    script

--- includes/builders/builders.php
+++ includes/builders/builders.php
@@ -38,7 +38,7 @@ if (!class_exists('AM_LottiePlayer_Builders')) {
         'dotlottie-player-light',
         AM_LOTTIEPLAYER_URL . 'scripts/dotlottie-player-light.min.js',
         null,
-        '2.2.5',
+        '2.3.2',
         [
           'strategy' => 'defer',
           'in_footer' => true
