f0a82b10225633e02957a57c7ce65e7aa9994111
johanaarstein <johan@aarstein.media>
2024-03-20 12:43:41 +0100

    new version

---
+++ svn/tags/3.1.0/build/lottieplayer/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'cf4fe362293ad66100aa');
