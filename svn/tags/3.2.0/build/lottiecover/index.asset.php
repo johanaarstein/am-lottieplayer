f6de68aa9bc0bca92ec46cfbb9fe24b89e5db8b5
Johan Martin Aarstein <johan@aarstein.media>
2024-08-06 21:30:43 +0200

    misc

--- svn/tags/3.2.0/build/lottiecover/index.asset.php
+++ svn/tags/3.2.0/build/lottiecover/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '4735410d128a0c55f0fc');
+<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => '53efa27a23fea437710f');
--- svn/tags/3.2.0/build/lottieplayer/index.asset.php
+++ svn/tags/3.2.0/build/lottieplayer/index.asset.php
@@ -1,1 +1,1 @@
-<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'bf25c7f045591a96a5a6');
+<?php return array('dependencies' => array('react', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f0d5fc86748cbc40bf1c');
