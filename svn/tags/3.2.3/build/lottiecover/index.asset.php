c95182ec549006438a45df477728bc30990cec11
Johan Martin Aarstein <johan@aarstein.media>
2024-09-11 18:24:33 +0200

    stash

---
+++ svn/tags/3.2.3/build/lottiecover/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'f4d427c1a69e04a717dd');
---
+++ svn/tags/3.2.3/build/lottieplayer/index.asset.php
@@ -0,0 +1,1 @@
+<?php return array('dependencies' => array('react-jsx-runtime', 'wp-blob', 'wp-block-editor', 'wp-blocks', 'wp-components', 'wp-data', 'wp-element', 'wp-i18n'), 'version' => 'c10994bcc124c10ed3c2');
