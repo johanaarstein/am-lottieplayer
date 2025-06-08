2ad8b31bf656eb9db2b28788a049c67ad3758b3e
Johan Martin Aarstein <johan@aarstein.media>
2025-06-08 23:58:57 +0200

    5.2.26

--- svn/trunk/am-lottieplayer.php
+++ svn/trunk/am-lottieplayer.php
@@ -12,7 +12,7 @@
  * Description:       The most complete, free Lottie Player plugin! It´s lightweight, versatile and easy to use – and has integrations for Gutenberg, Divi, Elementor, Flatsome and WPBakery.
  * Requires at least: 5.9
  * Requires PHP:      7.0
- * Version:           3.5.12
+ * Version:           3.5.13
  * Plugin URI:        https://www.aarstein.media/en/am-lottieplayer
  * Author:            Aarstein Media
  * Author URI:        https://www.aarstein.media/en
@@ -42,7 +42,7 @@ if ( ! \class_exists( 'AAMD_Lottie' ) ) {
 
 	class AAMD_Lottie {
 
-		private const _version = '3.5.12';
+		private const _version = '3.5.13';
 
 		/**
 		 * Sets up Am LottiePlayer plugin
